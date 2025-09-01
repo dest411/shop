ce9642d6eb5c76a8eea471741a5bece824bb809a
dest411 <vadimstrizhak652@gmail.com>
2025-09-02 00:30:15 +0300

    css

--- src/components/Shares.jsx
+++ src/components/Shares.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import '../css/Shares.css'
-import column from '../png/column.svg'
+import column from '../png/column.png'
 
 const Shares = () => {
   return (
     <div className='shares' >
-      <div>
-        <h1>Розумна колонка</h1>
-        <h2>Знижка 30%</h2>
-        <h3>при купівлі другого товара</h3>
+      <div className='shares-text' >
+        <p className='f' >Розумна колонка</p>
+        <p className='t' >Знижка 30%</p>
+        <p className='th' >при купівлі другого товара</p>
       </div>
       <div>
         <img src={column} alt="" />
