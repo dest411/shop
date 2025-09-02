572ba38f8c028084381ddc815de80d55b1b4c2f4
dest411 <vadimstrizhak652@gmail.com>
2025-09-02 15:13:01 +0300

    catalog

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import Shares from './components/Shares';
 import './App.css'
+import Catalog from './components/Catalog';
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Shares/>
+        <Catalog/>
     </div>
   );
 }
---
+++ src/components/Catalog.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Catalog = () => {
+  return (
+    <div>
+        <p>Каталог</p>
+        <div>
+            <div>
+                <div className='backg' ></div>
+                <div className='backg' ></div>
+                <div className='backg' ></div>
+                <div className='backg' ></div>
+                <div className='backg' ></div>
+                <div className='backg' ></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Catalog
