fa48b00c98f506da76d1e79e54753eac80058b06
dest411 <vadimstrizhak652@gmail.com>
2025-09-02 15:35:27 +0300

    catalog

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
+import '../css/Catalog.css'
+import image1 from '../png/image 1.png'
 
 const Catalog = () => {
   return (
-    <div>
-        <p>Каталог</p>
+    <div className='catalog' >
+        <h6>Каталог</h6>
         <div>
-            <div>
-                <div className='backg' ></div>
-                <div className='backg' ></div>
-                <div className='backg' ></div>
-                <div className='backg' ></div>
-                <div className='backg' ></div>
-                <div className='backg' ></div>
+            <div className='catalog-list' >
+                <div className='item' >
+                    <div className='backg' >
+                        <img src={image1} alt="" />
+                    </div>
+                    <p>Смартфони</p>
+                </div>
+                <div className='item' >
+                    <div className='backg' ></div>
+                    <p>Ноутбуки</p>
+                </div>
+                <div className='item' >
+                    <div className='backg' ></div>
+                    <p>Комп'ютери</p>
+                </div>
+                <div className='item' >
+                    <div className='backg' ></div>
+                    <p>Телевізори</p>
+                </div>
+                <div className='item' >
+                    <div className='backg' ></div>
+                    <p>Планшети</p>
+                </div>
+                <div className='item' >
+                    <div className='backg' ></div>
+                    <p>Колонки</p>
+                </div>
+                
             </div>
         </div>
     </div>
