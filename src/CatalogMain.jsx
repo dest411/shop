91883c06034ea7ec4487d0dd673c6a24443e6a7b
dest411 <vadimstrizhak652@gmail.com>
2025-09-06 23:06:20 +0300

    comm

--- src/CatalogMain.jsx
+++ src/CatalogMain.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Info from './componentsMain/Info'
+
 
 const CatalogMain = () => {
   return (
     <div>
         <Header />
+        <Info/>
     </div>
   )
 }
