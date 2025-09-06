80a3202977924cbf0930429579958dc216805c5c
dest411 <vadimstrizhak652@gmail.com>
2025-09-06 23:26:38 +0300

    css

--- src/componentsMain/Info.jsx
+++ src/componentsMain/Info.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../cssInfo/Info.css'
+import backBlack from '../png/backblack.svg'
 
 const Info = () => {
   return (
     <div className='info'  >
-        hello
+        <div className='linkTo' >
+            <Link to="/App" style={{ textDecoration: 'none', color: 'black', opacity: "60%" }} >Головна / </Link>
+            <p>Смартфони</p>
+        </div>
+        <div className='titleInfo' >
+            <Link to="/App" style={{ textDecoration: 'none', color: 'black', display:'flex', gap:"15px", alignItems: 'center' }} >
+                <img src={backBlack} alt="" />
+                <h6>Смартфони</h6>
+            </Link>
+        
+        </div>
     </div>
   )
 }
