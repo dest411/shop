2f0dbba7cc356aa4ca012352b707e1f267ecb96d
dest411 <vadimstrizhak652@gmail.com>
2025-10-03 19:24:53 +0300

    від до

--- src/componentsMain/Aside.jsx
+++ src/componentsMain/Aside.jsx
@@ -4,12 +4,17 @@ import '../cssInfo/MainCatalog.css'
 const Aside = () => {
   return (
     <div>
-        <aside>
+        <aside className='rounded-sm' >
             <div>
                 <p>Ціна, грн</p>
-                <div className='asideinput ' >
-                    <input type="text" />
-                    <input type="text" />
+                <div className='asideinput flex justify-between items-center mt-3' >
+                    <input type="text" placeholder='від'  className='
+                       border px-2 py-5 bg-lightBlue text-black 
+                      rounded-lg placeholder-black/70 focus:border-blue-400 outline-0 transition-colors duration-300'   />
+                      <span className='w-40 h-[1px] bg-black/20' ></span>
+                    <input type="text" placeholder='до' className='
+                      border px-2 py-5 bg-lightBlue text-black 
+                      rounded-lg placeholder-black/70 focus:border-blue-400 outline-0 transition-colors duration-300'/>
                 </div>
             </div>
       </aside>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        lightBlue: "#BCC5FF",
+        
+      }
+    },
   },
   plugins: [],
 }
